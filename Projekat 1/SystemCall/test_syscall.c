60bac6c39d17762817693d3ccc1ed1486bd43d6f
Stefan Marinkovic <stefanmarinkovic98ks@gmail.com>
2026-03-02 21:47:03 +0100

    Dodati snimci ekrana rezultata

--- Projekat 1/SystemCall/test_syscall.c
+++ Projekat 1/SystemCall/test_syscall.c
@@ -1,41 +1,59 @@
-#define _GNU_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/syscall.h>
-#include <errno.h>
 #include <string.h>
-#include <sys/types.h>
+#include <errno.h>
 
-#define __NR_find_procs_by_file 449
+#define SYS_find_procs_by_file 449
+#define MAX_PROCS 1024
 
 struct proc_info {
     pid_t pid;
+    int priority;
+    int nice;
+    uid_t uid;
+    unsigned long vm_size;
+    unsigned long utime;
+    unsigned long stime;
 };
 
 int main(int argc, char *argv[])
 {
     if (argc != 2) {
-        printf("Usage: %s <path>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <file_path>\n", argv[0]);
         return 1;
     }
 
-    struct proc_info buffer[128];
+    const char *path = argv[1];
+    struct proc_info results[MAX_PROCS];
 
-    long ret = syscall(__NR_find_procs_by_file,
-                       argv[1],
-                       buffer);
+    long ret = syscall(SYS_find_procs_by_file, path, results);
 
     if (ret < 0) {
-        printf("Syscall failed: %s (errno=%d)\n",
-               strerror(errno), errno);
+        perror("syscall failed");
         return 1;
     }
 
-    printf("Syscall returned %ld entries\n", ret);
+    if (ret == 0) {
+        printf("No processes are using file: %s\n", path);
+        return 0;
+    }
+
+    printf("Processes using file: %s\n\n", path);
 
     for (int i = 0; i < ret; i++) {
-        printf("PID: %d\n", buffer[i].pid);
+        printf("PID: %d\n", results[i].pid);
+        printf("Priority: %d\n", results[i].priority);
+        printf("Nice: %d\n", results[i].nice);
+        printf("UID: %d\n", results[i].uid);
+        printf("Virtual Memory: %lu bytes\n", results[i].vm_size);
+        printf("User Time: %lu\n", results[i].utime);
+        printf("System Time: %lu\n", results[i].stime);
+        printf("---------------------------------\n");
     }
+
+    printf("Total processes found: %ld\n", ret);
 
     return 0;
 }
